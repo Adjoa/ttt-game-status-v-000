4fbd53e469ef940a89d932239aa71958d1254aa2
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-16 17:38:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,47 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],  # top row
+  [3,4,5],  # middle row
+  [6,7,8],  # bottom row
+  [0,3,6],  # left column
+  [1,4,7],  # middle column
+  [2,5,8],  # right column
+  [0,4,8],  #left diagonal
+  [2,4,6]   #right diagonal
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    player = board[win_index_1]
+
+    if position_taken?(board, win_index_1)
+      win_found = win_combination.all?{|win_index| board[win_index] == player}
+
+      if win_found
+        return win_combination
+      end
+    end
+  end
+
+  return false
+end
+
+def full?(board)
+  not_full = board.any?{|player| player == nil || player == " "}
+  not_full ? false : true
+end
+
+
+def draw?(board)
+  puts "#{board}"
+  won = won?(board)
+  full = full?(board)
+
+ return !won && full
+end
+# Test.
+# board = ["X", " ", "O", " ", "X", "O", " ", " ", "X"]
+# won?(board)
